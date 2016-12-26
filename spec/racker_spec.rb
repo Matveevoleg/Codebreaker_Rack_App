047a7bca2365873ad353c8e4a9c5c026be3dd4fe
Matveevoleg <johndoe@example.com>
2016-12-26 15:23:28 +0200

    added tests

--- spec/racker_spec.rb
+++ spec/racker_spec.rb
@@ -8,4 +8,28 @@ RSpec.describe Racker do
       expect(subject.get(item).status).to eq 200
     end
   end
+
+  specify 'get responce 404' do
+    expect(subject.get('/qwerty').status).to eq 404
+  end
+
+  specify 'check buttons' do
+    expect(subject.get('/').body).to match(/<input type="submit" value="Start">/)
+    expect(subject.get('/start').body).to match(/<input type="submit" value="Check">/)
+  end
+
+  specify 'links' do
+    expect(subject.get('/start').body).to match(/<a href="\/get_hint">Get hint<\/a>/)
+    expect(subject.get('/start').body).to match(/<a href="\/start">Restart<\/a>/)
+    expect(subject.get('/start').body).to match(/<a href="\/">Exit<\/a>/)
+  end
+
+  specify 'attempts, hints count' do
+    expect(subject.get('/input').body).to match(/Attempts: 5/)
+    expect(subject.get('/input').body).to match(/Hints: 2/)
+  end
+
+  specify 'hints changed' do
+    expect(subject.get('/get_hint').body).to match(/Hints: 1/)
+  end
 end
